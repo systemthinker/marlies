464c15e80ca8d18a32260e3836850ff37d7d2b64
systemthinker <robbertoutenaar@gmail.com>
2020-05-15 08:37:35 +0200

    background and midsection + css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import './home.css'
 
 export default function (){
-    return  <div data-aos='fade-up'>
-    Helloooo!
+    return  (<div>
+
+    <div class="container-fluid ">
+      <div className="bg">
+        <div className="header">
+          <div>
+            <h1>Nature Academy</h1>
+          </div>
+          <div>
+            <p>Persoonlijke ontwikkeling in de natuur</p>
+          </div>
+          <div>
+          <button class="btn btn-light">Lees Meer</button>
+          </div>
+        </div>
+      </div>
+    
+      <div class="container">
+        <div class="row">
+          <div class="col">1</div>
+          <div class="col-xl-6 midSec">
+              <h1>Natuur is goed voor ons</h1>
+              <p>We weten het. Natuur maakt gelukkig, creatief en productief. De realiteit voor studenten en young professionals is vaak anders. De meeste tijd besteed je binnen aan werk, studie en contact met vrienden en familie.</p>
+              <p>Het is tijd om je mee naar buiten te nemen. Los te komen van de waan van de dag. Stilstaan en reflecteren op waar je staat en waar je naartoe wilt. Werken aan een thema dat belangrijk voor je is. Waar kan dat beter dan in de natuur?</p>
+              <p>Nature Academy faciliteert de ontwikkeling van waardevolle life skills in een groene omgeving. Weg van het klassieke klaslokaal.</p>
+          </div>    
+          <div class="col">3</div>     
+        </div>
+        </div>
+     
+     
+      </div>
+      
+
+
+
+  
+
   </div>
+
+
+    )
 }
